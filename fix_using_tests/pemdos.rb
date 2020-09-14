6b1a4295a948f518b3647248ab8fcb8fa9a895be
Patricia Arnedo <paaarnedo@gmail.com>
2020-09-14 19:06:22 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,19 @@
 # don't forget to add: require 'pry'
 
+require 'pry'
+
 def snake_it_up(string)
-  if string[0] == "s"
-    10 * "s" + string
-  else
-    string
+  
+  count = 1 
+  if string[0] == "s" 
+    
+    while count < 11 do 
+      string = "s" + string 
+      count += 1
+    end
+  p string
+  
   end
+  
 end
+
