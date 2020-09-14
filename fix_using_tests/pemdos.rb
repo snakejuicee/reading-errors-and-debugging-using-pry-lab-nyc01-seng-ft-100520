2cf5b44a0392f773e11bbe5b498f04303d88a9c1
Patricia Arnedo <paaarnedo@gmail.com>
2020-09-14 18:58:53 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -3,13 +3,16 @@
 require 'pry'
 
 def snake_it_up(string)
-  count = 0 
   
-  if string[0] == "s" do
+  count = 0 
+  if string[0] == "s" 
+    
     while count < 11 do 
+      string = "s" + string 
+      count += 1
       
-  else
+     else
   string
   end
-  p string
+  string
 end
